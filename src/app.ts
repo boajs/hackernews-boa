64df6d8b9a39384b0e63518bac322fed46c45172
bouzuya <m@bouzuya.net>
2016-04-20 20:37:50 +0900

    add news initial state

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import { A, O, Handler } from 'boa-core';
 
 const app: Handler = (action$: O<A<any>>, options?: any): O<A<any>> => {
-  return O.of({}).map(data => ({ type: 'render', data }));
+  const initialState = {
+    currentPage: 'news',
+    news: {
+      page: 1,
+      items: []
+    }
+  };
+  return O.of(initialState).map(data => ({ type: 'render', data }));
 };
 
 export { app };
