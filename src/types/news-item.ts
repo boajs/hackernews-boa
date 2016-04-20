c0a0c99a74a2ebb82fac70e1aa44e97e5605a977
bouzuya <m@bouzuya.net>
2016-04-20 20:37:41 +0900

    add types/news{-item}

---
+++ src/types/news-item.ts
@@ -0,0 +1,5 @@
+type NewsItem = {
+
+};
+
+export { NewsItem };
---
+++ src/types/news.ts
@@ -0,0 +1,8 @@
+import { NewsItem } from '../types/news-item';
+
+type News = {
+  page: number;
+  items: NewsItem[];
+};
+
+export { News };
