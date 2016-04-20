c27aed914e20c3f5a679ddb53fac40ea0f0be087
bouzuya <m@bouzuya.net>
2016-04-20 22:16:04 +0900

    change types/news-item & add dummy data

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,24 @@
 import { A, O, Handler } from 'boa-core';
+import { NewsItem } from './types/news-item';
 
 const app: Handler = (action$: O<A<any>>, options?: any): O<A<any>> => {
+  const newsItem: NewsItem = {
+    by: 'by',
+    descendants: 3,
+    id: 'id',
+    score: 123,
+    time: 'time',
+    title: 'title',
+    type: 'story',
+    url: 'http://example.com'
+  };
   const initialState = {
     currentPage: 'news',
     news: {
       page: 1,
-      items: []
+      items: [
+        newsItem
+      ]
     }
   };
   return O.of(initialState).map(data => ({ type: 'render', data }));
