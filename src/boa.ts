f2093176724fbb81d3eb7ca833929df9da0235d1
bouzuya <m@bouzuya.net>
2016-04-28 23:12:44 +0900

    add path-changed action behavior

--- src/boa.ts
+++ src/boa.ts
@@ -4,5 +4,6 @@ import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/merge';
 import 'rxjs/add/operator/scan';
 import 'rxjs/add/operator/share';
