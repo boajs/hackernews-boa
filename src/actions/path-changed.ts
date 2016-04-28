bb4a85315bcdd4e79143041bc1cc5c989b8a32c2
bouzuya <m@bouzuya.net>
2016-04-28 23:08:00 +0900

    add path-changed action

---
+++ src/actions/path-changed.ts
@@ -0,0 +1,17 @@
+import { makeSimpleAction } from '../make-simple-action';
+
+type Route = {
+  path: string;
+  [x: string]: any;
+};
+
+type MatchedRoute = {
+  route: Route;
+  params: {
+    [name: string]: string;
+  }
+};
+
+const { extract } = makeSimpleAction<MatchedRoute>('path-changed');
+
+export { extract };
