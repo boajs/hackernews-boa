5aee12d785a55f7c0e20e01e4dfe2959d70019c0
bouzuya <m@bouzuya.net>
2016-05-03 21:32:50 +0900

    fix item view

--- src/views/item-view.ts
+++ src/views/item-view.ts
@@ -3,9 +3,10 @@ import { view as itemView } from '../views/item';
 
 const view = ({ item, comments, pollOptions }: any, helpers: any): any => {
   const { create: h } = helpers;
+  if (!item) return null;
   const isJob = item.type === 'job';
   const hasText = item.hasOwnProperty('text');
-  return item ? h('div.view.item-view', [
+  return h('div.view.item-view', [
     itemView({ item }, helpers),
     hasText ? h('p.itemtext', [item.text]) : null,
     pollOptions ? h('ul.poll-options', pollOptions.map(option => {
@@ -18,7 +19,7 @@ const view = ({ item, comments, pollOptions }: any, helpers: any): any => {
       return commentView({ comment }, helpers);
     })) : null,
     comments.length === 0 && !isJob ? h('p', ['No comments yet.']) : null
-  ]) : null;
+  ]);
 };
 
 export { view };
