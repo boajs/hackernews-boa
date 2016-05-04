910668d4afda27068f1c6ab629f13721bf2d592f
bouzuya <m@bouzuya.net>
2016-05-04 23:44:13 +0900

    update item-view

--- src/views/item-view.ts
+++ src/views/item-view.ts
@@ -1,25 +1,73 @@
 import { view as commentView } from '../views/comment';
 import { view as itemView } from '../views/item';
 
-const view = ({ item, comments, pollOptions }: any, helpers: any): any => {
-  const { create: h } = helpers;
+type State = any;
+
+type ViewState = {
+  allComments: any[];
+  comments: any[];
+  hasComments: boolean;
+  hasNoComments: boolean;
+  hasPollOptions: boolean;
+  hasText: boolean;
+  item: any;
+  pollOptions: { text: string; score: string; }[];
+  text: string;
+};
+
+const viewState = ({
+  item, comments, pollOptions
+}: State, helpers: any): ViewState => {
   if (!item) return null;
-  const isJob = item.type === 'job';
-  const hasText = item.hasOwnProperty('text');
+  const hasComments = item.kids && item.kids.length > 0;
+  const hasPollOptions = !!pollOptions;
+  return {
+    allComments: comments,
+    comments: hasComments ? item.kids.map(id => comments[id]) : [],
+    hasComments,
+    hasNoComments: comments.length === 0 && item.type !== 'job',
+    hasPollOptions,
+    hasText: item.hasOwnProperty('text'),
+    item,
+    text: item.hasOwnProperty('text') ? item.text : null,
+    pollOptions: hasPollOptions ? pollOptions.map(i => {
+      return {
+        text: i.text,
+        score: i.score + ' points'
+      };
+    }) : []
+  };
+};
+
+const render = (state: ViewState, helpers: any): any => {
+  if (!state) return null;
+  const { create: h } = helpers;
   return h('div.view.item-view', [
-    itemView({ item }, helpers),
-    hasText ? h('p.itemtext', { innerHTML: item.text }) : null,
-    pollOptions ? h('ul.poll-options', pollOptions.map(option => {
-      return h('li', [
-        h('p', { innerHTML: option.text }),
-        h('p.subtext', [option.score, ' points'])
-      ]);
-    })) : null,
-    comments ? h('ul.comments', comments.map(comment => {
-      return commentView({ comment }, helpers);
-    })) : null,
-    comments.length === 0 && !isJob ? h('p', ['No comments yet.']) : null
+    itemView({ item: state.item }, helpers),
+    !state.hasText
+      ? null
+      : h('p.itemtext', { innerHTML: state.text }),
+    !state.hasPollOptions
+      ? null
+      : h('ul.poll-options', state.pollOptions.map(option => {
+        return h('li', [
+          h('p', { innerHTML: option.text }),
+          h('p.subtext', [option.score])
+        ]);
+      })),
+    !state.hasComments
+      ? null
+      : h('ul.comments', state.comments.map(comment => {
+        return commentView({ comment, comments: state.allComments }, helpers);
+      })),
+    !state.hasNoComments
+      ? null
+      : h('p', ['No comments yet.'])
   ]);
+};
+
+const view = (state: State, helpers: any): any => {
+  return render(viewState(state, helpers), helpers);
 };
 
 export { view };
