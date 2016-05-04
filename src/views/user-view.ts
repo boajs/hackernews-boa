f93a647e5d3a4268c931a28067d326d409dd7ab6
bouzuya <m@bouzuya.net>
2016-05-04 22:38:08 +0900

    refactor user-view

--- src/views/user-view.ts
+++ src/views/user-view.ts
@@ -1,42 +1,51 @@
-import { User } from '../types/user';
 import { fromNow } from '../views/helpers';
 
-const view = ({ user }: { user: User }, helpers: any): any => {
-  const { create: h } = helpers;
+type State = any;
+
+type ViewState = {
+  id: string;
+  created: string;
+  karma: string;
+  about: string;
+  submissionsUrl: string;
+  commentsUrl: string;
+};
+
+const viewState = ({ user }: State, _: any): ViewState => {
   if (!user) return null;
+  return {
+    id: user.id,
+    created: fromNow(user.created) + ' ago',
+    karma: String(user.karma),
+    about: user.about,
+    submissionsUrl: 'https://news.ycombinator.com/submitted?id=' + user.id,
+    commentsUrl: 'https://news.ycombinator.com/threads?id=' + user.id
+  };
+};
+
+const render = (state: ViewState, helpers: any): any => {
+  if (!state) return null;
+  const { create: h } = helpers;
   return h('div.view.user-view', [
     h('ul', [
-      h('li', [
-        h('span.label', ['user:']),
-        ' ',
-        user.id
-      ]),
-      h('li', [
-        h('span.label', ['created:']),
-        ' ',
-        fromNow(user.created),
-        ' ago'
-      ]),
-      h('li', [
-        h('span.label', ['karma:']),
-        ' ',
-        user.karma
-      ]),
+      h('li', [h('span.label', ['user:']), ' ', state.id]),
+      h('li', [h('span.label', ['created:']), ' ', state.created]),
+      h('li', [h('span.label', ['karma:']), ' ', state.karma]),
       h('li', [
         h('span.label', ['about:']),
-        h('div.about', { innerHTML: user.about })
+        h('div.about', { innerHTML: state.about })
       ])
     ]),
     h('p.links', [
-      h('a', { href: 'https://news.ycombinator.com/submitted?id=' + user.id }, [
-        'submissions'
-      ]),
+      h('a', { href: state.submissionsUrl }, ['submissions']),
       h('br'),
-      h('a', { href: 'https://news.ycombinator.com/threads?id=' + user.id }, [
-        'comments'
-      ])
+      h('a', { href: state.commentsUrl }, ['comments'])
     ])
   ]);
+};
+
+const view = (state: State, helpers: any): any => {
+  return render(viewState(state, helpers), helpers);
 };
 
 export { view };
