bb80fbe964be93af44f51e1529a850a4950a3b00
bouzuya <m@bouzuya.net>
2016-05-05 15:07:04 +0900

    refactor comment

--- src/views/comment.ts
+++ src/views/comment.ts
@@ -1,30 +1,64 @@
 import { pluralize, fromNow } from '../views/helpers';
 
-const view = ({ comment }: any, helpers: any): any => {
-  const { create: h } = helpers;
+type State = {
+  comment: any;
+  comments: any[];
+};
 
-  const childComments = [];
+type ViewState = {
+  by: string;
+  childComments: any[];
+  comments: any[];
+  hasChildComments: boolean;
+  text: string;
+  time: string;
+  url: string;
+};
+
+const viewState = (state: State, helpers: any): ViewState => {
+  if (!state) return null;
+  const { comment, comments } = state;
+  if (!comment) return null;
+  if (comment.type !== 'comment') return null;
+  const childComments = comment.kids
+    ? comment.kids.map(id => comments[id])
+    : [];
   const open = true;
 
-  // store.fetchItems(comment.kids).then(comments => {
-  //   childComments = comments;
-  // });
-  const href = '#/user/' + comment.by;
-  if (!comment.text) return null;
+  return {
+    by: comment.by,
+    childComments,
+    comments,
+    hasChildComments: childComments.length > 0,
+    text: comment.text,
+    time: fromNow(comment.time) + ' ago',
+    url: '#/user/' + comment.by
+  };
+}
+
+
+const render = (state: ViewState, helpers: any): any => {
+  if (!state) return null;
+  const { create: h } = helpers;
   return h('li', [
     h('div.comhead', [
       h('a.toggle', [ // TODO: click
         open ? '[-]' : '[+]'
       ]),
-      h('a', { href }, [comment.by]),
-      fromNow(comment.time),
-      ' ago'
+      h('a', { href: state.url }, [state.by]),
+      state.time
     ]),
-    h('p.comment-content', { innerHTML: comment.text }),
-    comment.kids ? h('ul.child-comments', childComments.map(comment => {
-      return view({ comment }, helpers);
-    })) : null
+    h('p.comment-content', { innerHTML: state.text }),
+    !state.hasChildComments
+      ? null
+      : h('ul.child-comments', state.childComments.map(comment => {
+        return view({ comment, comments: state.comments }, helpers);
+      }))
   ]);
+};
+
+const view = (state: State, helpers: any): any => {
+  return render(viewState(state, helpers), helpers);
 };
 
 export { view };
